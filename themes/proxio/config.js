f7969b2e0c42f8d0400b08dfd6e658b3fe731d26
workingemmettyoung-a11y <working.emmettyoung@gmail.com>
2025-09-03 16:44:42 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -188,9 +188,9 @@ const CONFIG = {
   PROXIO_CTA_BUTTON: true, // 是否显示按钮
   PROXIO_CTA_BUTTON_URL: '/about',
   PROXIO_CTA_BUTTON_TEXT: '联系我',
-
+  
   PROXIO_POST_REDIRECT_ENABLE: true, // 默認開啟重定向
   PROXIO_POST_REDIRECT_URL: 'https://www.work2death.icu/', // 重定向域名
   PROXIO_NEWSLETTER: process.env.NEXT_PUBLIC_THEME_PROXIO_NEWSLETTER || false // 是否开启邮件订阅 请先配置mailchimp功能 https://docs.tangly1024.com/article/notion-next-mailchimp
-}
-export default CONFIG
+},]
+export default CONFIG 
